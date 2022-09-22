a0c1620e2f8b8359b512a732e3fb5946c0d366e8
yimi-fighting <1348986909@qq.com>
2022-09-22 18:05:01 +0800

    轮播图

--- main.js
+++ main.js
@@ -2,6 +2,26 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 按需导入$http对象
+import {$http} from "@escook/request-miniprogram"
+
+// 将对象挂载到uni对象上
+uni.$http=$http
+
+// 配置请求的根路径
+// $http.baseUrl='https://www.uinav.com'
+$http.baseUrl='https://api-ugo-web.itheima.net'
+
+// 请求拦截器
+$http.beforeRequest=function(options){
+  uni.showLoading({
+    title:'数据加载中'
+  })
+}
+// 响应拦截器
+$http.afterRequest=function(options){
+  uni.hideLoading()
+}
 
 Vue.config.productionTip = false
 
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,15 @@
 <template>
   <view>
-    home
+    <!-- 首页的轮播图 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+          <!-- 循环渲染轮播图的 item 项 -->
+          <swiper-item v-for="(item, i) in swiperList" :key="i">
+            <view class="swiper-item">
+              <!-- 动态绑定图片的 src 属性 -->
+              <image :src="item.image_src"></image>
+            </view>
+          </swiper-item>
+        </swiper>
   </view>
 </template>
 
@@ -8,12 +17,37 @@
   export default {
     data() {
       return {
-        
+        swiperList:[]
       };
+    },
+    onLoad() {
+      this.getSwiperList()
+    },
+    methods:{
+      async getSwiperList(){
+        // 发送请求
+        const {data:res}= await uni.$http.get('/api/public/v1/home/swiperdata')
+        if(res.meta.status!==200){
+          // 说明请求失败
+          uni.showToast({
+            title:'数据请求失败',
+            icon:'none'
+          })
+        }
+        // 否则请求成功，修改data中的swiperList
+        this.swiperList=res.message
+      }
     }
   }
 </script>
 
 <style lang="scss">
+  swiper{
+    height: 300rpx;
+    .swiper-item,image{
+      width: 100%;
+      height: 100%;
+    }
+  }
 
 </style>
